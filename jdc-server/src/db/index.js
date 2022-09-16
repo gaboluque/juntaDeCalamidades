2a3cadc349c9e911c5d17a3ad53d31dd377a3be3
gabolque <gluquemiranda@gmail.com>
2022-09-16 17:23:15 -0500

    Solve db

--- jdc-server/src/db/index.js
+++ jdc-server/src/db/index.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb');
 
 
 const dbName = "jdc_" + process.env.NODE_ENV;
-const uri = "mongodb+srv://gaboluque:JLFq1lZvPJM8UYZ7@cluster0.dxeiklm.mongodb.net/" + dbName;
+const uri = process.env.MONGO_URI + dbName;
 const client = new MongoClient(uri);
 
 async function connectDB() {
